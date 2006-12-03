bb9f20bd718c405553b0780e010805cb244175aa
Shawn O. Pearce <spearce@spearce.org>
2006-12-02 20:52:49 -0500

    Removed unnecessary leading whitespace from Log program.
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.spearce.jgit/src/org/spearce/jgit/pgm/Log.java
+++ org.spearce.jgit/src/org/spearce/jgit/pgm/Log.java
@@ -1,23 +1,23 @@
-    package org.spearce.jgit.pgm;
+package org.spearce.jgit.pgm;
 
-    import java.io.File;
-    import java.io.IOException;
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 
 import org.spearce.jgit.lib.Commit;
 import org.spearce.jgit.lib.Repository;
 
-    public class Log {
-        public static void main(String[] args) throws IOException {
-            Repository db = new Repository(new File(".git"));
-            Commit commit = db.mapCommit(args[0]);
-            System.out.println("commit "+commit.getCommitId());
-            System.out.println("tree "+commit.getTreeId());
-            for (Iterator ci=commit.getParentIds().iterator(); ci.hasNext(); ) {
-                System.out.println("parent "+ci.next());
-            }
-            System.out.println("author "+commit.getAuthor());
-            System.out.println();
-            System.out.println(commit.getMessage());
-        }
+public class Log {
+    public static void main(String[] args) throws IOException {
+	Repository db = new Repository(new File(".git"));
+	Commit commit = db.mapCommit(args[0]);
+	System.out.println("commit " + commit.getCommitId());
+	System.out.println("tree " + commit.getTreeId());
+	for (Iterator ci = commit.getParentIds().iterator(); ci.hasNext();) {
+	    System.out.println("parent " + ci.next());
+	}
+	System.out.println("author " + commit.getAuthor());
+	System.out.println();
+	System.out.println(commit.getMessage());
     }
+}
