c3907f5c5976e4e22dd3327b7eb8f5e811ec68c8
Shawn O. Pearce <spearce@spearce.org>
2006-12-04 11:37:57 -0500

    Removed unused imports from Repository class.
    These imports became unused when I converted the pack collection into
    a simple array, to improve performance of pack access.
    
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.spearce.jgit/src/org/spearce/jgit/lib/Repository.java
+++ org.spearce.jgit/src/org/spearce/jgit/lib/Repository.java
@@ -25,8 +25,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
 
 import org.spearce.jgit.errors.IncorrectObjectTypeException;
 import org.spearce.jgit.errors.ObjectWritingException;
