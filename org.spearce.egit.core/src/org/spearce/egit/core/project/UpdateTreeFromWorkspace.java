71da23d2096be3c72430ad439038b95ce0c9c220
Shawn O. Pearce <spearce@spearce.org>
2007-01-29 21:37:19 -0500

    Document and cleanup the UpdateTreeFromWorkspace visitor.
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.spearce.egit.core/src/org/spearce/egit/core/project/UpdateTreeFromWorkspace.java
+++ org.spearce.egit.core/src/org/spearce/egit/core/project/UpdateTreeFromWorkspace.java
@@ -26,49 +26,85 @@ import org.spearce.jgit.lib.SymlinkTreeEntry;
 import org.spearce.jgit.lib.Tree;
 import org.spearce.jgit.lib.TreeVisitor;
 
+/**
+ * Resynchronize a JGit {@link Tree} with part of the Eclipse workspace.
+ * <p>
+ * Updates the JGit Tree to more closely match the current status of the Eclipse
+ * workspace container it was given.
+ * </p>
+ * <p>
+ * The update rules are quite simple:
+ * </p>
+ * <ul>
+ * <li>If JGit has a subtree, but Eclipse doesn't have that same name as a
+ * container, delete the JGit subtree.</li>
+ * <li>If JGit has a blob, but Eclipse doesn't have a file by that same name,
+ * delete the JGit blob.</li>
+ * <li>If both JGit and Eclipse have a blob/tree at the same name, mark the
+ * JGit blob modified. This allows a future execution of {@link CheckpointJob}
+ * to rehash the file data and determine the correct object name for its
+ * contents.</li>
+ * <li>Ignore symlinks in the JGit tree.</li>
+ * </ul>
+ * <p>
+ * Note that because every blob/file is marked as modified, the entire JGit Tree
+ * will need to be written back out to the Git object database before any sort
+ * of meaningful comparsion with another JGit Tree can be performed.
+ * </p>
+ */
 public class UpdateTreeFromWorkspace implements TreeVisitor {
 	private IContainer currentContainer;
 
+	/**
+	 * Create a new update tree visitor.
+	 * 
+	 * @param root
+	 *            the Eclipse workspace container which corresponds with the
+	 *            JGit {@link Tree} object this visitor will be handed to.
+	 */
 	public UpdateTreeFromWorkspace(final IContainer root) {
 		currentContainer = root;
 	}
 
 	public void startVisitTree(final Tree t) throws IOException {
-		if (!t.isRoot()) {
-			final IResource r = currentContainer.findMember(t.getName());
-			IContainer c;
-
-			if (r == null) {
-				c = null;
-			} else if (r instanceof IContainer) {
-				c = (IContainer) r;
-			} else {
-				c = (IContainer) r.getAdapter(IContainer.class);
-			}
-
-			if (c == null || !c.exists()) {
-				t.delete();
-			} else {
-				currentContainer = c;
-			}
+		if (t.isRoot()) {
+			// JGit visits the root tree, but it has no name. It
+			// should corresponding with the IContainer given to
+			// us when we were created. So do nothing, and let
+			// JGit call us again for each item within the root
+			// tree.
+			//
+			return;
 		}
+
+		final IResource r = currentContainer.findMember(t.getName());
+		final IContainer c;
+		if (r == null)
+			c = null;
+		else if (r instanceof IContainer)
+			c = (IContainer) r;
+		else
+			c = (IContainer) r.getAdapter(IContainer.class);
+
+		if (c == null || !c.exists())
+			t.delete();
+		else
+			currentContainer = c;
 	}
 
 	public void endVisitTree(final Tree t) throws IOException {
-		if (t.getParent() != null) {
+		if (t.getParent() != null)
 			currentContainer = currentContainer.getParent();
-		}
 	}
 
 	public void visitFile(final FileTreeEntry f) throws IOException {
 		final IResource r = currentContainer.findMember(f.getName());
-		if (r == null || !r.exists()) {
+		if (r == null || !r.exists())
 			f.delete();
-		} else if (!(r instanceof IFile) && r.getAdapter(IFile.class) == null) {
+		else if (!(r instanceof IFile) && r.getAdapter(IFile.class) == null)
 			f.delete();
-		} else {
+		else
 			f.setModified();
-		}
 	}
 
 	public void visitSymlink(final SymlinkTreeEntry s) throws IOException {
