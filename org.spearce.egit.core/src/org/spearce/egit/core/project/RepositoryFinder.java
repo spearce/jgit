46e302065876a291068ffef273c7b11eca2f2a51
Shawn O. Pearce <spearce@spearce.org>
2007-01-29 21:27:27 -0500

    Document the RepositoryFinder class.
    I also discovered that the register method is not used outside of the
    class, so I'm changing its access to be private.  It probably should
    have been private from the beginning.
    
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.spearce.egit.core/src/org/spearce/egit/core/project/RepositoryFinder.java
+++ org.spearce.egit.core/src/org/spearce/egit/core/project/RepositoryFinder.java
@@ -31,16 +31,54 @@ import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.SubProgressMonitor;
 import org.spearce.egit.core.CoreText;
 
+/**
+ * Searches for existing Git repositories associated with a project's files.
+ * <p>
+ * This finder algorithm searches a project's contained files to see if any of
+ * them are located within the working directory of an existing Git repository.
+ * The finder searches through linked resources, as the EGit core is capable of
+ * dealing with linked directories spanning multiple repositories in the same
+ * project.
+ * </p>
+ * <p>
+ * The search algorithm is exhaustive, it will find all matching repositories.
+ * For the project itself as well as for each linked container within the
+ * project it scans down the local filesystem trees to locate any Git
+ * repositories which may be found there. It also scans up the local filesystem
+ * tree to locate any Git repository which may be outside of Eclipse's
+ * workspace-view of the world, but which contains the project or a linked
+ * resource within the project. In short, if there is a Git repository
+ * associated, it finds it.
+ * </p>
+ */
 public class RepositoryFinder {
 	private final IProject proj;
 
 	private final Collection results;
 
+	/**
+	 * Create a new finder to locate Git repositories for a project.
+	 * 
+	 * @param p
+	 *            the project this new finder should locate the existing Git
+	 *            repositories of.
+	 */
 	public RepositoryFinder(final IProject p) {
 		proj = p;
 		results = new ArrayList();
 	}
 
+	/**
+	 * Run the search algorithm.
+	 * 
+	 * @param m
+	 *            a progress monitor to report feedback to; may be null.
+	 * @return all found {@link RepositoryMapping} instances associated with the
+	 *         project supplied to this instance's constructor.
+	 * @throws CoreException
+	 *             Eclipse was unable to access its workspace, and threw up on
+	 *             us. We're throwing it back at the caller.
+	 */
 	public Collection find(IProgressMonitor m) throws CoreException {
 		if (m == null) {
 			m = new NullProgressMonitor();
@@ -102,7 +140,7 @@ public class RepositoryFinder {
 		return new File(new File(fsLoc, ".git"), "config");
 	}
 
-	public void register(final IContainer c, final File gitdir,
+	private void register(final IContainer c, final File gitdir,
 			final String subset) {
 		File f;
 		try {
