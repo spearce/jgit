5d212d070bd70dd8927c7871ade3b2f0801467be
Dave Watson <dwatson@mimvista.com>
2007-06-28 10:04:52 -0400

    Approximately 10x speedup for finding changed files
    Since we've already done an index-head comparison, we can skip that step
    for each file in the filesystem trees we're walking.
    
    This change, on an unpacked repository with nearly 40000 files (mostly in
    .git/objects), took the time from 16 seconds to 1.5 seconds.
    
    Signed-off-by: Dave Watson <dwatson@mimvista.com>

--- org.spearce.egit.ui/src/org/spearce/egit/ui/internal/actions/CommitAction.java
+++ org.spearce.egit.ui/src/org/spearce/egit/ui/internal/actions/CommitAction.java
@@ -483,16 +483,29 @@ public class CommitAction implements IObjectActionDelegate {
 			RepositoryMapping repositoryMapping = projectData
 					.getRepositoryMapping(resource.getProject());
 
-			if (repositoryMapping.isResourceChanged(resource)) {
+			if (isChanged(repositoryMapping, resource)) {
 				files.add(resource);
 				category.add(resource);
 				return true;
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	private boolean isChanged(RepositoryMapping map, IFile resource) {
+		try {
+			Repository repository = map.getRepository();
+			GitIndex index = repository.getIndex();
+			String repoRelativePath = map.getRepoRelativePath(resource);
+			Entry entry = index.getEntry(repoRelativePath);
+			if (entry != null)
+				return entry.isModified(map.getWorkDir());
+			return false;
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return false;
