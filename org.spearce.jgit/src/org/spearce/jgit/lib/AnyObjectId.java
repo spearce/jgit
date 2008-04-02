470e61609717edfe2527e97997172b6ccabee060
Shawn O. Pearce <spearce@spearce.org>
2008-04-01 23:18:25 -0400

    Add support to ObjectId for copying raw bytes to a byte[]
    By copying into a byte array callers may be able to pool writing
    the object id with other writes, or at least batch together all
    of the virtual method calls to OutputStream.write into a single
    method call.
    
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.spearce.jgit/src/org/spearce/jgit/lib/AnyObjectId.java
+++ org.spearce.jgit/src/org/spearce/jgit/lib/AnyObjectId.java
@@ -234,6 +234,22 @@ public abstract class AnyObjectId implements Comparable {
 	}
 
 	/**
+	 * Copy this ObjectId to a byte array.
+	 * 
+	 * @param b
+	 *            the buffer to copy to.
+	 * @param o
+	 *            the offset within b to write at.
+	 */
+	public void copyRawTo(final byte[] b, final int o) {
+		NB.encodeInt32(b, o, w1);
+		NB.encodeInt32(b, o + 4, w2);
+		NB.encodeInt32(b, o + 8, w3);
+		NB.encodeInt32(b, o + 12, w4);
+		NB.encodeInt32(b, o + 16, w5);
+	}
+
+	/**
 	 * Copy this ObjectId to an output writer in raw binary.
 	 * 
 	 * @param w
