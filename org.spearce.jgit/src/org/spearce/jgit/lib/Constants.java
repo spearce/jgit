8b9ac8d4a8c2dc98fa46b534f84ec0293ee221c1
Robin Rosenberg <robin.rosenberg@dewire.com>
2008-04-08 07:42:47 +0200

    Fixed wrong name for remotes refs
    Signed-off-by: Robin Rosenberg <robin.rosenberg@dewire.com>

--- org.spearce.jgit/src/org/spearce/jgit/lib/Constants.java
+++ org.spearce.jgit/src/org/spearce/jgit/lib/Constants.java
@@ -161,7 +161,7 @@ public final class Constants {
 	public static final String HEADS_PREFIX = "refs/heads";
 
 	/** Prefix for remotes refs */
-	public static String REMOTES_PREFIX = "remotes";
+	public static String REMOTES_PREFIX = "refs/remotes";
 
 	/**
 	 * Create a new digest function for objects.
