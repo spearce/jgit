6fb537f6a7f99622df9fe0537700f69fc3ccd22f
Grzegorz Kossakowski <grek@google.com>
2009-08-11 18:16:09 -0700

    Add PathSuffixFilter to match TreeWalk paths by suffix
    Added a simple TreeFilter that filters all entries that have path
    with given suffix. This filter is always recursive.
    
    Signed-off-by: Grzegorz Kossakowski <grek@google.com>
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.spearce.jgit/src/org/spearce/jgit/treewalk/TreeWalk.java
+++ org.spearce.jgit/src/org/spearce/jgit/treewalk/TreeWalk.java
@@ -739,6 +739,37 @@ public class TreeWalk {
 	}
 
 	/**
+	 * Test if the supplied path matches (being suffix of) the current entry's
+	 * path.
+	 * <p>
+	 * This method tests that the supplied path is exactly equal to the current
+	 * entry, or is relative to one of entry's parent directories. It is faster
+	 * to use this method then to use {@link #getPathString()} to first create
+	 * a String object, then test <code>endsWith</code> or some other type of
+	 * string match function.
+	 *
+	 * @param p
+	 *            path buffer to test.
+	 * @param pLen
+	 *            number of bytes from <code>buf</code> to test.
+	 * @return true if p is suffix of the current path;
+	 *         false if otherwise
+	 */
+	public boolean isPathSuffix(final byte[] p, final int pLen) {
+		final AbstractTreeIterator t = currentHead;
+		final byte[] c = t.path;
+		final int cLen = t.pathLen;
+		int ci;
+
+		for (ci = 1; ci < cLen && ci < pLen; ci++) {
+			if (c[cLen-ci] != p[pLen-ci])
+				return false;
+		}
+
+		return true;
+	}
+
+	/**
 	 * Get the current subtree depth of this walker.
 	 *
 	 * @return the current subtree depth of this walker.
